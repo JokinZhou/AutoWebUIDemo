fc6b3c3134cc6e6efa804c48bbe1d6a32300fbc8
lenovo <Jokin911005>
2018-05-05 20:03:49 +0800

    再提交一次

--- src/main/java/util/WebActionUtil.java
+++ src/main/java/util/WebActionUtil.java
@@ -16,6 +16,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
  * @author jokin
  *把所有的web操作方法等集成封装到这来，selenium原生web操作和开源的特殊操作方式
  *（目前用来是给actionFactory来调用具体执行具体的测试操作，也可在单个独立testcase里面调用）
+ *selenium的三种等待方式待封装进来
  *
  */
 public class WebActionUtil {
@@ -94,7 +95,7 @@ public class WebActionUtil {
         // 获取所有页面的句柄，并循环判断不是当前的句柄   
         for (String temhandle : driver.getWindowHandles()) {    
             if (!temhandle.equals(handle))    
-                //driver.close();  
+                //driver.close();//每次切换窗口都关闭之前的窗口，确保之后打开新窗口都能是保持只有两个窗口，使这个方法可以重复调用
                 driver= driver.switchTo().window(temhandle);   
              
         }
